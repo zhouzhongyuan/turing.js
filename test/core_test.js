d26f81a13663f8897c765d6730fc843c9e36860b
zhouzhongyuan <yawenzhongyuan@gmail.com>
2016-11-28 15:26:34 +0800

    core test

---
+++ test/core_test.js
@@ -0,0 +1,13 @@
+if (typeof  load != 'undefined'){
+    load('riot.js');
+    load('../turing.core.js');
+}
+Riot.context('turing.core.js',function () {
+    given('the turing object', function () {
+        should('be gloabal and accessible', turing).isNotNull();
+        should('return a VERSION', turing.VERSION).isNotNull();
+        should('be turing complete', true).isTrue();
+    });
+})
+
+Riot.run();
